8613ed04f37f4ff53fc48f091234b9727729a9f2
ashutoshsao <ashutoshsao17@gmail.com>
2025-11-24 16:27:21 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+type Bindings = {
+  DATABASE_URL: string;
+};
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.post('/', async (c) => {
+  // Todo add zod validation here
+  const body: {
+    name: string;
+    email: string;
+    password: string
+  } = await c.req.json()
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  return c.json({ msg: "as" })
+})
+
+export default app
